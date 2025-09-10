3e93a274e2642e8cb34822f66952a2ac77eac4e2
rafli zocky <raflizocky@gmail.com>
2025-09-10 20:52:09 +0700

    fix cors

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Customer extends Model
+class Customer extends Authenticatable
 {
     use HasApiTokens;
 
