7ce685609e711ee6fe3da4a5b2dde5d737ebfcd6
rafli zocky <raflizocky@gmail.com>
2025-09-12 20:40:14 +0700

    remove login manual view

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            // ->login()
+            ->login()
             ->colors([
                 'primary' => Color::Amber,
             ])
--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/user', fn() => 'User Dashboard')->name('user.dashboard');
-});
